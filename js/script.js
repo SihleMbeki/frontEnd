671d20245a75c86b374b9e8dd1b3716c71a9a776
Masakhane Matanzima <sihle.matanzima@gmail.com>
2022-10-10 20:45:25 +0100

    Updates

--- js/script.js
+++ js/script.js
@@ -73,3 +73,90 @@ $.ajax({
 
 });
 
+
+$('#registerForm').submit(function(e){
+	e.preventDefault();
+	//Read the username
+	var username;
+	var password;
+	var password2;
+	var email;
+	
+	password=$("#inputPassword").val();
+	password2=$("#inputPasswordC").val();
+	if(password!=password2){
+		$("#error").text("Password does not match");
+		return false;
+	}
+	email=$("#inputEmail").val();
+	username=$("#inputUsername").val()
+	// Stop the form
+	var sendingData = {
+		Username: username,
+		Email: email,
+		Password: password
+	 }
+
+	 var json = JSON.stringify(sendingData);
+
+$.ajax({  
+	type: "POST",  
+		url: "http://localhost:5001/api/account/register",
+		headers: {
+			'Accept': 'application/json',
+      'Content-Type': 'application/json'
+
+		},
+		crossDomain: true,
+		data: json,    
+		success: function (data, textStatus, xhr) {
+			if(data.role=="public"){
+				window.location.assign("http://localhost/view/view_home.html");
+			}
+			 if(data.role=="admin"){
+			window.location.assign("http://localhost/view/admin_home.html");
+		}
+		
+			console.log(data);
+			console.log(xhr.status);
+			localStorage.setItem('user', JSON.stringify(data));
+		}, 
+		failure: function(response) {  
+			$("#error").text("Username taken");
+			console.log(response.d); 
+		},
+    complete: function(xhr, textStatus) {
+        console.log(xhr.status);
+		console.log(xhr);
+		if(xhr.status==401 || xhr.status==500){
+			$("#error").html("Invalid Login Credentials");
+			//$("#error").toggleClass(".error");
+		}
+		$("#error").text(textStatus);
+    }
+	}); /*
+	 $.post('http://localhost:5001/api/account/login', json, function(response){ 
+      alert("success");
+     // $("#mypar").html(response.amount);
+});/*
+	$.ajax({
+		url: 'http://localhost:5001/api/account/login',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Access-Control-Allow-Origin': 'http://localhost/view'
+		},
+		type: "POST", /* or type:"GET" or type:"PUT" 
+		dataType: 'jsonp',
+		data: {
+			sendingData
+		},
+		success: function (result) {
+			console.log(result);
+		},
+		error: function () {
+			console.log("error");
+		}
+	});*/
+	
+
+});
--- script.js
+++ script.js
@@ -41,7 +41,6 @@ $.ajax({
 			//$("#error").toggleClass(".error");
 		}
     } 
- 
 	}); /*
 	 $.post('http://localhost:5001/api/account/login', json, function(response){ 
       alert("success");
@@ -67,4 +66,50 @@ $.ajax({
 	});*/
 	
 
+});
+
+
+$('#loginForm').submit(function(e){
+	e.preventDefault();
+	//Read the username
+	var username;
+	var password;
+	password=$("#inputPassword").val();
+	username=$("#inputUsername").val()
+	// Stop the form
+	var sendingData = {
+		Username: username,
+		Password: password
+	 }
+
+	 var json = JSON.stringify(sendingData);
+
+$.ajax({  
+		type: "POST",  
+		url: "http://localhost:5001/api/account/register",
+		headers: {
+			'Accept': 'application/json',
+      'Content-Type': 'application/json'
+
+		},
+		crossDomain: true,
+		data: json,    
+		success: function (data, textStatus, xhr) {
+			window.location.assign("http://localhost/view/admin_home.html")
+			console.log(data);
+			console.log(xhr.status);
+			localStorage.setItem('user', JSON.stringify(data));
+		}, 
+		failure: function(response) {  
+			console.log(response.d); 
+		} 
+		,
+    complete: function(xhr, textStatus) {
+        console.log(xhr.status);
+		if(xhr.status==401 || xhr.status==500){
+			$("#error").html("Invalid Login Credentials");
+			//$("#error").toggleClass(".error");
+		}
+    } 
+	}); 
 });
